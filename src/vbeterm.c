b4e3c6df87b1bac20ef933dc46c8400065678f23
Vincent Bernat <vincent@bernat.im>
2014-09-25 20:28:38 +0200

    term: simplify setting visual

--- src/vbeterm.c
+++ src/vbeterm.c
@@ -184,10 +184,8 @@ main(int argc, char *argv[])
 
 	/* Pack widgets and start the terminal */
 	gtk_container_add(GTK_CONTAINER(window), terminal);
-	gtk_widget_set_app_paintable(window, TRUE);
-	GdkScreen *screen = gtk_widget_get_screen(window);
-	GdkVisual *visual = gdk_screen_get_rgba_visual(screen);
-	gtk_widget_set_visual(window, visual);
+	gtk_widget_set_visual(window, gdk_screen_get_rgba_visual(gtk_widget_get_screen(window)));
 	gtk_widget_show_all(window);
+	gtk_window_set_focus(GTK_WINDOW(window), terminal);
 	gtk_main();
 }
